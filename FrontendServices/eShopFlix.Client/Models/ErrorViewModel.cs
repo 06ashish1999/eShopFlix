c9da044425959dc508f4cd0bcb9d6fabb63b909a
Sai Ashish Pinnamsetty <ideas.something@gmail.com>
2024-10-16 16:43:18 +0530

    Add project files.

---
+++ FrontendServices/eShopFlix.Client/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eShopFlix.Client.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
