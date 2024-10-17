73300bb60efceecc2fe236082952c9efc3c3a5f5
Sai Ashish Pinnamsetty <ideas.something@gmail.com>
2024-10-17 10:28:20 +0530

    Implementing CleanArchitecture for AuthService

---
+++ BackendServices/eShopFlix.AuthService/eShopFlix.AuthService.Core/Repository.Contracts/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using eShopFlix.AuthService.Core.Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopFlix.AuthService.Core.Repository.Contracts
+{
+    public interface IUserRepository
+    {
+        User LogInUser(string email, string password);
+        bool RegisterUser(User user, Role role);
+    }
+}
---
+++ BackendServices/eShopFlix.AuthService/eShopFlix.AuthService.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using eShopFlix.AuthService.Core.Database.Entities;
+using eShopFlix.AuthService.Core.Repository.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopFlix.AuthService.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public User LogInUser(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RegisterUser(User user, Role role)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
